80072b7038dc199ddc9799ed0b82d59d704afdd4
Aze543 <eizen.inopia.9@gmail.com>
2024-08-08 22:31:33 +0800

    version 0.1.1

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ app.config['SECRET_KEY'] = "jwqdhgdigdqhg43%^dshighdjgdjghijh12345@$4dqjh$*90h"
 def home():
     return render_template('index.html')
 
+@app.route('/rent')
+def car_list():
+    return render_template('list.html')
+
+@app.route('/car-details')
+def car_details():
+    return '<h1>Refridgator</h1><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJSudXYuY7T-mmea-LeuKBBxDiihBaNcmbug&s" alt="..." />'
+
 
 if __name__ == "__main__":
     app.run(debug=True)
